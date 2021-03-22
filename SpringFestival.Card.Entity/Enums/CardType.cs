b8713eab4c60c97165b9b3a5251e891590482924
shixiaoyong <62409809+Christopher-Shi@users.noreply.github.com>
2021-03-22 18:03:07 +0800

    Update CardType.cs

--- SpringFestival.Card.Entity/Enums/CardType.cs
+++ SpringFestival.Card.Entity/Enums/CardType.cs
@@ -10,31 +10,31 @@ namespace SpringFestival.Card.Entity.Enums
         /// <summary>
         /// 歌曲类
         /// </summary>
-        [Description("歌曲类")] Song = 0,
+        [Description("歌曲类")] Song = 1,
 
         /// <summary>
         /// 舞蹈类
         /// </summary>
-        [Description("舞蹈类")] Dance = 1,
+        [Description("舞蹈类")] Dance = 2,
 
         /// <summary>
         /// 语言类
         /// </summary>
-        [Description("语言类")] Language = 2,
+        [Description("语言类")] Language = 3,
 
         /// <summary>
         /// 创意类
         /// </summary>
-        [Description("创意类")] Creative = 3,
+        [Description("创意类")] Creative = 4,
 
         /// <summary>
         /// 戏曲类
         /// </summary>
-        [Description("戏曲类")] Drama = 4,
+        [Description("戏曲类")] Drama = 5,
 
         /// <summary>
         /// 功夫类
         /// </summary>
-        [Description("功夫类")] kungFu = 5
+        [Description("功夫类")] kungFu = 6
     }
 }
